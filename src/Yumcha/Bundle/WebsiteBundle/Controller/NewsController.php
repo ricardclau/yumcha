e6a6d0a488ed95c0d39fcfa2753bd2820f3a43f9
Ricard Clau <ricard.clau@gmail.com>
2013-04-19 17:01:36 +0200

    news feed from Facebook

--- src/Yumcha/Bundle/WebsiteBundle/Controller/NewsController.php
+++ src/Yumcha/Bundle/WebsiteBundle/Controller/NewsController.php
@@ -14,6 +14,19 @@ class NewsController extends Controller
      */
     public function indexAction()
     {
-        return [];
+        $apcKey = 'yumcha_fb_news';
+
+        if (apc_exists($apcKey)) {
+            $news = apc_fetch($apcKey);
+        } else {
+            $news = json_decode($this->get('buzz')->get(
+                'https://www.facebook.com/feeds/page.php?id=211061718986660&format=json',
+                ['User-Agent' => $this->getRequest()->headers->get('User-Agent')]
+            )->getContent());
+
+            apc_store($apcKey, $news, 1800);
+        }
+
+        return ['news' => $news];
     }
 }
