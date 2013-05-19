8ae307df0992ed082c2f1ef50871280989269ff8
Ricard Clau <ricard.clau@gmail.com>
2013-05-19 18:36:27 +0100

    news section

--- src/Yumcha/Bundle/WebsiteBundle/Controller/NewsController.php
+++ src/Yumcha/Bundle/WebsiteBundle/Controller/NewsController.php
@@ -23,10 +23,29 @@ class NewsController extends Controller
                 'https://www.facebook.com/feeds/page.php?id=211061718986660&format=json',
                 ['User-Agent' => $this->getRequest()->headers->get('User-Agent')]
             )->getContent());
-
             apc_store($apcKey, $news, 1800);
         }
 
-        return ['news' => $news];
+        $newsInfo = [];
+        foreach ($news->entries as $new) {
+            if (
+                (1 === preg_match('/<img.*src=\"(\S*)\"/', $new->content, $matches)) &&
+                (false === strpos($new->content, 'fbexternal'))
+            ) {
+                $tmp = str_replace('_s', '_n', $matches[1]);
+                $pos = strrpos($tmp, '/');
+                $img = substr($tmp, 0, $pos) . '/p200x200' . substr($tmp, $pos);
+            } else {
+                $img = "/img/default_news.png";
+            }
+
+            $newsInfo[] = [
+                'title' => $new->title,
+                'img'   => $img,
+                'link'  => $new->alternate,
+            ];
+        }
+
+        return ['news' => $newsInfo];
     }
 }
